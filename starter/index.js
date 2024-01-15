bdd3b3f9423d36c359cd39bb7a5c66fc4cc5bc91
ctjoseph1 <79760037+ctjoseph1@users.noreply.github.com>
2024-01-15 19:13:46 +0000

    started array for questions and content for contents section in readme. Tested first array and appears in readme

--- starter/index.js
+++ starter/index.js
@@ -3,18 +3,33 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+// array of questions for user inquirer 9/10
 const questions = [
+{
+type:'input',
+message: 'What is the title of your project?',
+name: 'title',
+},
+{
+    type:'input',
+    message: 'What is the title of your project?',
+    name: 'title',   
+}
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data)
 }
 
 // function to initialize program
 function init() {
-
+inquirer.prompt(questions)
+.then (data => {
+    const readme= generateMarkdown(data)
+    writeToFile('./utils/readme.md', readme)
+})
 }
 
 // function call to initialize program
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -2,6 +2,17 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Table of contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3.
+## Installation
+
+${data.installation}
+
+## Usage
+${data.usage}
+
 `;
 }
 
