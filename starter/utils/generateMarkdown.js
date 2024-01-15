8b27a43437bb719fef3e7db0cdaec9d078c55be9
ctjoseph1 <79760037+ctjoseph1@users.noreply.github.com>
2024-01-15 19:24:22 +0000

    corrected order of markdown js file

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -3,15 +3,32 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
 ## Table of contents
-1. [Installation](#installation)
-2. [Usage](#usage)
-3.
-## Installation
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
 
+
+## Installation
 ${data.installation}
 
 ## Usage
 ${data.usage}
+
+## License
+${data.license}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.questions}
 
 `;
 }
